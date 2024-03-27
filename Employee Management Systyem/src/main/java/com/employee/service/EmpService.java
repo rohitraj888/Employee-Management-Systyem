7e1784f460b0e496d2fd4d31f39f466d09073809
rohitraj888 <rohitraj17888@gmail.com>
2024-03-27 19:09:06 +0530

    add files

---
+++ Employee Management Systyem/src/main/java/com/employee/service/EmpService.java
@@ -0,0 +1,16 @@
+package com.employee.service;
+
+import java.util.List;
+
+import com.employee.entity.Employee;
+
+public interface EmpService {
+
+	public Employee saveEmp(Employee emp);
+	
+	public  List<Employee> getAllEmp();
+	
+	public Employee getEmpById(int id);
+	
+	public boolean deleteEmp(int id);
+}
